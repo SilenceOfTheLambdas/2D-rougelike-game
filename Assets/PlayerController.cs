67b89dbf4a4341e7acb8fc7e5e8fd3e0d6baed5f
Callum-James Smith <techdragon@tuta.io>
2020-02-01 18:18:27 +0000

    implemented basic movement and animator

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.5f;
+    private Vector2 direction;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        TakeInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * (speed * Time.deltaTime));
+        SetAnimatorMovement(direction);
+    }
+
+    private void TakeInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+    }
+
+    private void SetAnimatorMovement(Vector2 direction)
+    {
+        _animator.SetFloat("xDir", direction.x);
+        _animator.SetFloat("yDir", direction.y);
+        print(_animator.GetFloat("xDir"));
+    }
+}
