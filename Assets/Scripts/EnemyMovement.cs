4178fbcff2b7b80c3a450dde0653f9ae419da3fc
Callum-James Smith <techdragon@tuta.io>
2020-02-02 19:16:49 +0000

    Made a better test level, added collisions for walls

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -21,26 +21,25 @@ public class EnemyMovement : MonoBehaviour
 
     private void PlayAnimation()
     {
-
-        if (aiPath.desiredVelocity.x >= 0.01f)
+        if (aiPath.desiredVelocity.x > 0f)
         {
             // Enemy is moving right
             SetAnimatorMovement(new Vector2(1, 0));
         }
 
-        if (aiPath.desiredVelocity.x <= 0f)
+        if (aiPath.desiredVelocity.x < 0f)
         {
             // Enemy is moving to the left
             SetAnimatorMovement(new Vector2(-1, 0));
         }
 
-        if (aiPath.desiredVelocity.y >= 0.01f)
+        if (aiPath.desiredVelocity.y > 0f)
         {
             // Player is moving upwards
             SetAnimatorMovement(new Vector2(0, 1));
         }
 
-        if (aiPath.desiredVelocity.y <= 0f)
+        if (aiPath.desiredVelocity.y < 0f)
         {
             // Enemy is moving down
             SetAnimatorMovement(new Vector2(0, -1));
