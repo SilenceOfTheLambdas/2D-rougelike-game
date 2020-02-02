c4127cc56399a94dd7347e70c91b8716c45ebf4d
Callum-James Smith <techdragon@tuta.io>
2020-02-02 14:52:42 +0000

    Fixed player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,22 @@ public class PlayerController : MonoBehaviour
     private Animator _animator;
     private static readonly int XDir = Animator.StringToHash("xDir");
     private static readonly int YDir = Animator.StringToHash("yDir");
+    private static readonly int facingDown = Animator.StringToHash("FacingDown");
+    private static readonly int facingUp = Animator.StringToHash("FacingUp");
+    private static readonly int facingLeft = Animator.StringToHash("FacingLeft");
+    private static readonly int facingRight = Animator.StringToHash("FacingRight");
 
+    /*Player Enum showing which direction the player is facing*/
+    private enum Facing
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    [SerializeField]private Facing facingDirection = Facing.Down; // The default direction the player is facing
+    
     private void Start()
     {
         _animator = GetComponent<Animator>();
@@ -25,7 +40,7 @@ public class PlayerController : MonoBehaviour
 
         if (_direction.x != 0 || _direction.y != 0)
         {
-            SetAnimatorMovement(_direction);
+            SetAnimatorMovement(facingDirection);
         }
         else
         {
@@ -40,26 +55,36 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             _direction += Vector2.up;
+            facingDirection = Facing.Up;
+            SetAnimatorMovement(Facing.Up);
         }
         if (Input.GetKey(KeyCode.A))
         {
             _direction += Vector2.left;
+            facingDirection = Facing.Left;
+            SetAnimatorMovement(Facing.Left);
         }
         if (Input.GetKey(KeyCode.S))
         {
             _direction += Vector2.down;
+            facingDirection = Facing.Down;
+            SetAnimatorMovement(Facing.Down);
         }
         if (Input.GetKey(KeyCode.D))
         {
             _direction += Vector2.right;
+            facingDirection = Facing.Right;
+            SetAnimatorMovement(Facing.Right);
         }
     }
 
-    private void SetAnimatorMovement(Vector2 d)
+    private void SetAnimatorMovement(Facing direction)
     {
         _animator.SetLayerWeight(1, 1); // Set the layer animation to 1, so it plays
-        _animator.SetFloat(XDir, d.x);
-        _animator.SetFloat(YDir, d.y);
+        _animator.SetBool(facingLeft, direction == Facing.Left);
+        _animator.SetBool(facingRight, direction == Facing.Right);
+        _animator.SetBool(facingDown, direction == Facing.Down);
+        _animator.SetBool(facingUp, direction == Facing.Up);
     }
 
     public static void TakeDamage(float amount)
